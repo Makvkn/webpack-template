5d3c7beb9a72cdfdda692898304f90847b1a9e31
maksim makovkin <maksim.makovkin@neuro-city.ru>
2023-06-27 16:15:34 +0300

    configs

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './styles.css'
+import IMAGE from './react.svg'
+export const App = () => {
+    return <>
+        <h1>React TypeScript Webpack Starter Template</h1>
+        <img src={IMAGE} alt="React Logo" width="300" height="200"/>
+    </>
+}
